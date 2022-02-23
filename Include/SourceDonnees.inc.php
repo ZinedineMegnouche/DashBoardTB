8d24f584c3a8941da16d67bb700c131b3f841649
Zinedine MEGNOUCHE <79139880+ZinedineZX@users.noreply.github.com>
2022-02-23 10:13:04 +0100

    Update SourceDonnees.inc.php

--- Include/SourceDonnees.inc.php
+++ Include/SourceDonnees.inc.php
@@ -2,7 +2,7 @@
 function SGBDConnect()
 {
     try {
-       $connexion = new PDO('mysql:host=localhost;dbname=library', 'root', 'root'); 
+       $connexion = new PDO('mysql:host=localhost;dbname=TickAndBox', 'root', 'root'); 
        $connexion->query('SET NAMES UTF8');
        $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);        
     } catch (PDOException $e) {
